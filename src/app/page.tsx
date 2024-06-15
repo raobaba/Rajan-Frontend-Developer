ae5ea1f99b29677a92bb97e06f5aa22643c17ff4
Rajan Kumar <raorajan9576@gmail.com>
2024-06-15 14:59:55 +0530

    Roadmap created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Navbar from "@/components/navbar";
 import LandingPage from "@/components/landing";
 import Tokenomics from "@/components/tokenomics";
-import WorkPage from "@/components/works"
+import WorkPage from "@/components/works";
+import RoadmapPage from "@/components/roadmap";
 export default function Home() {
   return (
     <main>
       <Navbar />
       <LandingPage />
-      <WorkPage/>
+      <WorkPage />
       <Tokenomics />
+      <RoadmapPage />
     </main>
   );
 }
--- src/components/roadmap.tsx
+++ src/components/roadmap.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
 
 function RoadmapPage() {
   return (
-    <div>RoadmapPage</div>
-  )
+    <div className="font-outfit mt-[-50px] justify-center  w-full bg-gradient-to-b from-black via-purple-900 to-black text-white">
+      <h1 className="text-center text-3xl font-bold text-purple-300 mb-8 lg:mb-16">
+        Roadmap
+      </h1>
+      <div className="flex justify-end">
+        <div className="w-10/12 flex justify-between h-80">
+          <div className="lg:w-6/12 lg:ml-8 lg:mt-10 lg:mr-0">
+            <h2 className="text-2xl font-bold text-yellow-400 mb-4">PHASE 1</h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>Horizen dai dai harmony dogecoin waves nexo.</li>
+              <li>Flow horizon waves dash tether zcash waves dash terraUSD.</li>
+              <li>
+                Quant harmony amps cosmos PancakeSwap decentralized decred.
+              </li>
+              <li>
+                Serum TRON solana father holo crypto. Flow eCash amp EOS
+                digibyte stellar.
+              </li>
+            </ul>
+          </div>
+          <img
+            src="./phase1.png"
+            alt="Phase 1 Image"
+            className="lg:w-1/3 lg:mt-8 lg:ml-0 lg:mr-8 object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-end">
+        <div className="w-10/12 flex justify-between h-80">
+          <div className="lg:w-6/12 lg:ml-8 lg:mt-10 lg:mr-0">
+            <h2 className="text-2xl font-bold text-yellow-400 mb-4">PHASE 1</h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>Horizen dai dai harmony dogecoin waves nexo.</li>
+              <li>Flow horizon waves dash tether zcash waves dash terraUSD.</li>
+              <li>
+                Quant harmony amps cosmos PancakeSwap decentralized decred.
+              </li>
+              <li>
+                Serum TRON solana father holo crypto. Flow eCash amp EOS
+                digibyte stellar.
+              </li>
+            </ul>
+          </div>
+          <img
+            src="./phase2.png"
+            alt="Phase 1 Image"
+            className="lg:w-1/3 lg:mt-8 lg:ml-0 lg:mr-8 object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="flex  justify-end">
+        <div className="w-10/12 flex justify-between h-80">
+          <div className="lg:w-6/12 lg:ml-8 lg:mt-10 lg:mr-0">
+            <h2 className="text-2xl font-bold text-yellow-400 mb-4">PHASE 1</h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>Horizen dai dai harmony dogecoin waves nexo.</li>
+              <li>Flow horizon waves dash tether zcash waves dash terraUSD.</li>
+              <li>
+                Quant harmony amps cosmos PancakeSwap decentralized decred.
+              </li>
+              <li>
+                Serum TRON solana father holo crypto. Flow eCash amp EOS
+                digibyte stellar.
+              </li>
+            </ul>
+          </div>
+          <img
+            src="./phase3.png"
+            alt="Phase 1 Image"
+            className="lg:w-1/3 lg:mt-8 lg:ml-0 lg:mr-8 object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default RoadmapPage
+export default RoadmapPage;
